f109dde3131a7d42fea5f62df56066b523565baf
Garret Patten <garret.patten@proton.me>
2026-03-09 20:32:01 -0400

    fix mobile scroll bug

--- src/router/index.js
+++ src/router/index.js
@@ -36,6 +36,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+
+    return { top: 0, left: 0 };
+  },
 });
 
 export default router;
