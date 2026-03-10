36ef4202925e69fd799c070fc7245df3bfb320c3
Garret Patten <garret.patten@proton.me>
2026-03-09 20:54:08 -0400

    replace All Rights Reserved with my favorite Djikstra quote

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,8 @@
     class="border-t border-cobalt-200 dark:border-cobalt-800 mt-16 pt-8 pb-8"
   >
     <p class="text-sm text-cobalt-800 dark:text-cobalt-200 text-center">
-      © {{ currentYear }} Garret Patten. All rights reserved.
+      "Simplicity is a great virtue, but it requires hard work to achieve it."
+      - Edsger Dijkstra
     </p>
   </footer>
 </template>
-
-<script setup>
-import { ref } from "vue";
-
-const currentYear = ref(new Date().getFullYear());
-</script>
